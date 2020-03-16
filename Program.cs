07cd5fdb7c8ccc8cfdef25a886f2ccc158b089f7
DanielGomes2769 <62227214+DanielGomes2769@users.noreply.github.com>
2020-03-15 23:46:26 -0300

    TesteGuide
    Activity done

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TesteGuide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testcase = "joao silva";
+
+            var firstName = "";
+            var lastName = "";
+            lastName = lastName.ToUpper();
+            var split = testcase.Split(new char[] { ' ' }, 2);
+            if (split.Length == 1)
+            {
+                firstName = "";
+                lastName = split[0];
+            }
+            else
+            {
+                firstName = split[0];
+                lastName = split[1];
+            }
+            System.Globalization.CultureInfo cultureinfo = System.Threading.Thread.CurrentThread.CurrentCulture;
+            Console.WriteLine(lastName.ToUpper() + ',' + cultureinfo.TextInfo.ToTitleCase(firstName));
+        }
+    }
+}
